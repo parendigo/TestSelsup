622522c6c950cb9d8f9cf756fda65cd054c3742f
parendigo <pam.artem@mail.ru>
2024-06-16 20:15:48 +0300

    1. Added commentaries 2. Made addNewCallNode synchronized 3. Minor changes on modifiers of fields

--- ConnectApi/src/main/java/study/CrptApi.java
+++ ConnectApi/src/main/java/study/CrptApi.java
@@ -8,57 +8,63 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
-
-public class SendJsonHelper {
+@SuppressWarnings("SpellCheckingInspection")
+public class CrptApi {
     private final long timeAmountMillis; // total refresh time
     private ListCallNode head; // List contains previous method calls
-    private ListCallNode tail;
+    private ListCallNode tail; // helps to add to the end of the List
+    private final int refreshRate = 10; // used to define frequency with which the List will be refreshed
     private final int requestLimit;
-    Semaphore semaphore; // contains requestAmount
+    private final Semaphore semaphore; // contains requestLimit
 
-//    private final String DefaultUrl = "https://ismp.crpt.ru/api/v3/lk/documents/create";
     private final String DefaultUrl = "https://ismp.crpt.ru/api/v3/lk/documents/create";
+    /*private final String DefaultUrl = "http://localhost:8080/test";*/
 
-    public SendJsonHelper(int requestLimit, TimeUnit timeUnit) {
+    public CrptApi(int requestLimit, TimeUnit timeUnit) {
         this.timeAmountMillis = timeUnit.toMillis(1);
         this.requestLimit = requestLimit;
         this.semaphore = new Semaphore(requestLimit, true);
     }
 
-    public SendJsonHelper(int requestLimit, long timeAmount, TimeUnit timeUnit) {
+    public CrptApi(int requestLimit, long timeAmount, TimeUnit timeUnit) {
         this.timeAmountMillis = timeUnit.toMillis(timeAmount);
         this.requestLimit = requestLimit;
         this.semaphore = new Semaphore(requestLimit, true);
     }
 
-    public <T> boolean sendJsonHttpPost(T json, String signature) {
-        if ((semaphore.availablePermits() * 10) % requestLimit == 0)
+    //  returns null, if post was not executed due to requestlimit.
+    //  returns body if status >= 200 && status < 300, otherwise returns status code.
+    //  Can make method also synchronized to keep the sequence of posts, but it will affect the execution speed
+    public <T> String sendJsonHttpPost(T json, String signature) {
+        if ((semaphore.availablePermits() * refreshRate) % requestLimit == 0)
             refreshCounterAndDeleteOldCalls();
         if (semaphore.tryAcquire()) {
             addNewCallNode();
-            sendJson(json, signature);
-            return true;
+            return sendJson(json, signature);
         }
-        return false;
+        return null;
     }
 
+    //  Deletes all old notes about method calls and releases semaphore
     private synchronized void refreshCounterAndDeleteOldCalls() {
         long currentTime = System.currentTimeMillis();
+        int count = 0;
         while (head != null && currentTime - head.callTime >= timeAmountMillis) {
             head = head.next;
-            semaphore.release();
+            count++;
         }
+        semaphore.release(count);
     }
 
-    private void addNewCallNode() {
+    private synchronized void addNewCallNode() {
         ListCallNode lcn = new ListCallNode(System.currentTimeMillis());
         if (head == null) {
             head = lcn;
@@ -69,7 +75,8 @@ public class SendJsonHelper {
         }
     }
 
-    private static class ListCallNode {
+    //  Contains nodes of recent method calls. Recent calls are added to the end
+    private class ListCallNode {
         long callTime;
         ListCallNode next;
 
@@ -79,14 +86,13 @@ public class SendJsonHelper {
     }
 
     private <T> String sendJson(T json, String signature) {
-
         try (CloseableHttpClient client = HttpClients.createDefault()) {
 
             HttpPost httpPost = new HttpPost(DefaultUrl);
             httpPost.setHeader("Content-Type", "application/json");
             httpPost.setHeader("Accept", "application/json");
             httpPost.setHeader("Authorization", "Bearer " + signature);
-            httpPost.setEntity(new StringEntity(parseJsonData(json)));
+            httpPost.setEntity(new StringEntity(JsonDataToString(json)));
 
             ResponseHandler<String> responseHandler = new MyResponseHandler();
             return client.execute(httpPost, responseHandler);
@@ -96,15 +102,14 @@ public class SendJsonHelper {
         return null;
     }
 
-    private String parseJsonData(String jsonString) {
-        return jsonString;
-    }
-
-    private String parseJsonData(JSONObject jsonObject) {
-        return jsonObject.toString();
-    }
-
-    private String parseJsonData(Object object) {
+    private <T> String JsonDataToString(T object) {
+        if (object instanceof String) {
+            System.out.println("String");
+            return (String) object;
+        } else if (object instanceof JSONObject) {
+            System.out.println("JSONObject");
+            return object.toString();
+        }
         ObjectMapper om = new ObjectMapper();
         StringWriter sw = new StringWriter();
         try {
@@ -125,7 +130,15 @@ public class SendJsonHelper {
                 if (entity == null) {
                     return Integer.toString(status);
                 } else {
-                    return EntityUtils.toString(entity);
+                    StringBuilder sb = new StringBuilder();
+                    try (InputStream in = entity.getContent()) {
+                        byte[] buffer = new byte[4096];
+                        while (in.available() > 0) {
+                            int count = in.read(buffer);
+                            sb.append(new String(buffer, 0, count));
+                        }
+                    }
+                    return sb.toString();
                 }
             } else return Integer.toString(status);
         }
